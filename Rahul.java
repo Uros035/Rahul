cf8f6b7a8ed5278e27cd9684cccc782bf392bd1d
Uros035 <81822235+Uros035@users.noreply.github.com>
2021-10-29 08:21:15 +0200

    Add files via upload

--- Rahul.java
+++ Rahul.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,7 +14,7 @@ public class Rahul {
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		int j=0;
-		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		String[] itemsNeeded= {"Cucumber","Brocolli","Beetroot","Carrot"}; 
 		
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
@@ -36,10 +37,19 @@ public class Rahul {
 		//click on  Add to cart
 		driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
 		if(j==itemsNeeded.length)
+		
 		{
          break;
 		}
 	}
+		//System.out.println(driver.findElement(By.id("results")).getText());
+        // css selektor "[ono sto pise u inspekt npr [id='start'] i posle npr buttin"
+
+		//driver.findElement(By.xpath("/html/body/div/div/header/div/div[3]/a[4]/img")).click();
+		//driver.findElement(By.xpath("/html/body/div/div/header/div/div[3]/div[2]/div[2]/button")).click();
+		//Thread.sleep(3000L);
+		//driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/input")).sendKeys("rahulshettyacademy");
+		//driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/button")).click();
 
 }
 }
